ab71485bb4728be0ef67af52aee5d433fbe68953
Natthapong Chantaklad <73196876+Alicisox@users.noreply.github.com>
2024-01-26 13:54:28 +1100

    Add the second solution (Comparison).

--- 242isAnagram.py
+++ 242isAnagram.py
@@ -19,6 +19,17 @@ class Solution:
                 continue
             return False
         return True
+    
+    def isAnagramv2(self, s: str, t: str) -> bool:
+        # Comparison method
+        if len(s) != len(t):
+            return False
+        sCount = {}
+        tCount = {}
+        for i in range(len(s)):
+            sCount[s[i]] = 1 + sCount.get(s[i], 0) 
+            tCount[t[i]] = 1 + tCount.get(t[i], 0)
+        return sCount == tCount
 
 # Testing
 s = "anagram"
@@ -28,3 +39,5 @@ s = "rat"
 t = "car"
 
 print(Solution().isAnagram(s, t))
+
+print(Solution().isAnagramv2(s, t))
